8d7a6ebf56db1b40996512b65862cf0c9134fa4c
3ygun <soller.23@osu.edu>
2015-04-15 00:15:25 -0400

    Complete with bad coordinates

--- maze.cpp
+++ maze.cpp
@@ -2,27 +2,76 @@
 #include <iomanip>
 using namespace std;
 
+#define theDim 3
+#define thePlayer '$'
 
-void setupMaze(char[][3][3]);
-int printMaze(char[][3][3], int[]);
+void setupMaze(char[][theDim][theDim]);
+void setupLocation(int[]);
+int printMaze(char[][theDim][theDim], int[]);
+void updateMaze(char[][theDim][theDim], int[], char);
 
 int main(void) {
-	char maze[3][3][3];				// z, y, x
-	int location[3] = {1, 1, 1};
+	char maze[theDim][theDim][theDim];				// z, y, x
+	int location[theDim];
 	int win = 0;
-	char move;
+	char move = 'q';
 
-	while(!win) {
-		setupMaze(maze);
+	// Now setup the maze
+	setupMaze(maze);
+	setupLocation(location);
 
+	// Print the instructions
+	cout << ": This is the 2D 3D Maze on Next Gen Command Line!" << endl;
+	cout << ":  By David Soller" << endl;
+	cout << ":" << endl;
+	cout << ": Controls are:" << endl;
+	cout << ":   u = up     ( +z  change )" << endl;
+	cout << ":   d = down   ( -z  change )" << endl;
+	cout << ":   i = in     ( +y  change )" << endl;
+	cout << ":   o = out    ( -y  change )" << endl;
+	cout << ":   r = right  ( +x  change )" << endl;
+	cout << ":   l = left   ( -x  change )" << endl;
+	cout << ":   q = quit   (  to  exit  )" << endl;
+	cout << ":" << endl;
+
+	// Prompt the user to continue
+	cout << ": Press a key to continue (q to Quit) ";
+	cin >> move;
+
+	// Try to escape the maze
+	while(move != 'q') {
+		// Print the current maze
 		win = printMaze(maze, location);
-		cin >> move;
+
+		// Determine whether another move needs to be made
+		if (!win) {
+			// Prompt for next move
+			cout << ": Choose move  ";
+			cin >> move;
+
+			// Act upon the choosing move
+			if (move != 'q') {
+				updateMaze(maze, location, move);
+			}
+		} else {
+			// They won! So tell them
+			cout << ": You escaped the maze! Congratulations!" << endl;
+			cout << ":  Press q to Quit or another key to play again?  ";
+			cin >> move;
+
+			// Playing again?
+			if (move != 'q') {
+				// Reset <(*.*<)
+				setupMaze(maze);
+				setupLocation(location);
+			}
+		}
 	}
 }
 
 
 
-void setupMaze(char maze[][3][3]) {
+void setupMaze(char maze[][theDim][theDim]) {
 	// z, y, x
 	// z = 0
 	maze[0][0][0] = '+';
@@ -40,7 +89,7 @@ void setupMaze(char maze[][3][3]) {
 	maze[1][0][1] = ' ';
 	maze[1][0][2] = '+';
 	maze[1][1][0] = '+';
-	maze[1][1][1] = '$';
+	maze[1][1][1] = thePlayer;
 	maze[1][1][2] = '+';
 	maze[1][2][0] = '+';
 	maze[1][2][1] = '+';
@@ -56,12 +105,17 @@ void setupMaze(char maze[][3][3]) {
 	maze[2][2][0] = '+';
 	maze[2][2][1] = '+';
 	maze[2][2][2] = '+';
-
 }
 
-int printMaze(char maze[][3][3], int location[]) {
+void setupLocation(int location[]) {
+	location[0] = 1;
+	location[1] = 1;
+	location[2] = 1;
+}
+
+int printMaze(char maze[][theDim][theDim], int location[]) {
 	int winner = 0;
-	int dim = 3; 	// Dimensions of maze
+	int dim = theDim; 	// Dimensions of maze
 	int hdim = 1;	// Half of the Dimension
 
 	// Find all of the edges
@@ -75,7 +129,6 @@ int printMaze(char maze[][3][3], int location[]) {
 
 	// Check for winner
 	if (((zmin < 0) || (ymin < 0) || (xmin < 0)) || ((zmax >= dim) || (ymax >= dim) || (xmax >= dim))) {
-		cout << "winner! \n \n";
 		winner = 1;
 	} else {
 		/*
@@ -87,10 +140,11 @@ int printMaze(char maze[][3][3], int location[]) {
 		int cx = location[2] - hdim;
 
 		// Print the header
-		cout << ":    Current position in ( z, y, x) is @(" << cz << ", " << cy << ", " << cx << ")" << endl;
-		cout << ":    Get to +-" << hdim << " on any axis to escape!" << endl;
 		cout << ":" << endl;
-		cout << ":    X      Z" << endl;
+		cout << ": Current position in ( z, y, x) is @(" << cz << ", " << cy << ", " << cx << ")" << endl;
+		cout << ": Get to +-" << hdim << " on any axis to escape!" << endl;
+		cout << ":" << endl;
+		cout << ":    Z      Y" << endl;
 
 
 
@@ -129,7 +183,7 @@ int printMaze(char maze[][3][3], int location[]) {
 
 		// Spacer & Headers
 		cout << ":" << endl;
-		cout << ":   / \\     Y" << endl;
+		cout << ":   / \\     X" << endl;
 
 		for (int i = 0; i < dim; ++i) {
 			// Create y point
@@ -140,9 +194,9 @@ int printMaze(char maze[][3][3], int location[]) {
 			// Print diagram top & row start
 			cout << ": ";
 			if ( i==0 ) {
-				cout << " / Z \\ ";
+				cout << " / Y \\ ";
 			} else if ( i==1 ) {
-				cout << "|\\ Z /|";
+				cout << "|\\ Y /|";
 			} else { // ( i==2 )
 				cout << "| \\ / |";
 			}
@@ -158,10 +212,121 @@ int printMaze(char maze[][3][3], int location[]) {
 		}
 
 		// Print bottom of diagram
-		cout << ": |XX|YY|" << endl;
+		cout << ": |ZZ|XX|" << endl;
 		cout << ":  \\ | / " << endl;
 		cout << ":   \\|/  " << endl;
+		cout << ":" << endl;
 	}
 
 	return winner;
+}
+
+void updateMaze(char maze[][theDim][theDim] , int location[], char move) {
+	int z = location[0];
+	int y = location[1];
+	int x = location[2];
+	int n = 0;
+
+	// See what their move was
+	switch (move) {
+		case 'u':
+			// Calculate the new coordinate
+			n = z + 1;
+
+			// Move the player if possible
+			if (maze[n][y][x] != '+') {
+				// It's possible update everything
+				maze[n][y][x] = thePlayer;
+				maze[z][y][x] = ' ';
+				location[0] = n;
+			} else {
+				// Invalid move hit a wall
+				cout << ":  Invalid move: " << move << endl;
+			}
+
+			break;
+		case 'd':
+			// Calculate the new coordinate
+			n = z - 1;
+
+			// Move the player if possible
+			if (maze[n][y][x] != '+') {
+				// It's possible update everything
+				maze[n][y][x] = thePlayer;
+				maze[z][y][x] = ' ';
+				location[0] = n;
+			} else {
+				// Invalid move hit a wall
+				cout << ":  Invalid move: " << move << endl;
+			}
+
+			break;
+		case 'i':
+			// Calculate the new coordinate
+			n = y + 1;
+
+			// Move the player if possible
+			if (maze[z][n][x] != '+') {
+				// It's possible update everything
+				maze[z][n][x] = thePlayer;
+				maze[z][y][x] = ' ';
+				location[1] = n;
+			} else {
+				// Invalid move hit a wall
+				cout << ":  Invalid move: " << move << endl;
+			}
+
+			break;
+		case 'o':
+			// Calculate the new coordinate
+			n = y - 1;
+
+			// Move the player if possible
+			if (maze[z][n][x] != '+') {
+				// It's possible update everything
+				maze[z][n][x] = thePlayer;
+				maze[z][y][x] = ' ';
+				location[1] = n;
+			} else {
+				// Invalid move hit a wall
+				cout << ":  Invalid move: " << move << endl;
+			}
+
+			break;
+		case 'r':
+			// Calculate the new coordinate
+			n = x + 1;
+
+			// Move the player if possible
+			if (maze[z][y][n] != '+') {
+				// It's possible update everything
+				maze[z][y][n] = thePlayer;
+				maze[z][y][x] = ' ';
+				location[2] = n;
+			} else {
+				// Invalid move hit a wall
+				cout << ":  Invalid move: " << move << endl;
+			}
+
+			break;
+		case 'l':
+			// Calculate the new coordinate
+			n = x - 1;
+
+			// Move the player if possible
+			if (maze[z][y][n] != '+') {
+				// It's possible update everything
+				maze[z][y][n] = thePlayer;
+				maze[z][y][x] = ' ';
+				location[2] = n;
+			} else {
+				// Invalid move hit a wall
+				cout << ":  Invalid move: " << move << endl;
+			}
+
+			break;
+		default:
+			cout << "ERROR: Bad move choice" << endl;
+			break;
+	}
 }

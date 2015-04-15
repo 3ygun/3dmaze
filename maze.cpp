6a6d7cb0dd5715e33aaef7659186505c5e1df7a6
3ygun <soller.23@osu.edu>
2015-04-14 22:18:23 -0400

    Switched header location & updated Maze

--- maze.cpp
+++ maze.cpp
@@ -26,35 +26,35 @@ void setupMaze(char maze[][3][3]) {
 	// z, y, x
 	// z = 0
 	maze[0][0][0] = '+';
-	maze[0][0][1] = '-';
+	maze[0][0][1] = '+';
 	maze[0][0][2] = '+';
-	maze[0][1][0] = '-';
-	maze[0][1][1] = '-';
-	maze[0][1][2] = '-';
+	maze[0][1][0] = '+';
+	maze[0][1][1] = '+';
+	maze[0][1][2] = '+';
 	maze[0][2][0] = '+';
-	maze[0][2][1] = '-';
+	maze[0][2][1] = '+';
 	maze[0][2][2] = '+';
 
 	// z = 1
 	maze[1][0][0] = '+';
 	maze[1][0][1] = ' ';
 	maze[1][0][2] = '+';
-	maze[1][1][0] = '-';
+	maze[1][1][0] = '+';
 	maze[1][1][1] = '$';
-	maze[1][1][2] = '-';
+	maze[1][1][2] = '+';
 	maze[1][2][0] = '+';
-	maze[1][2][1] = '-';
+	maze[1][2][1] = '+';
 	maze[1][2][2] = '+';
 
 	// z = 2
 	maze[2][0][0] = '+';
-	maze[2][0][1] = '-';
+	maze[2][0][1] = '+';
 	maze[2][0][2] = '+';
-	maze[2][1][0] = '-';
-	maze[2][1][1] = '-';
-	maze[2][1][2] = '-';
+	maze[2][1][0] = '+';
+	maze[2][1][1] = '+';
+	maze[2][1][2] = '+';
 	maze[2][2][0] = '+';
-	maze[2][2][1] = '-';
+	maze[2][2][1] = '+';
 	maze[2][2][2] = '+';
 
 }
@@ -78,6 +78,25 @@ int printMaze(char maze[][3][3], int location[]) {
 		cout << "winner! \n \n";
 		winner = 1;
 	} else {
+		/*
+		 *	Print current location stuff
+		 */
+		// Current location
+		int cz = location[0] - hdim;
+		int cy = location[1] - hdim;
+		int cx = location[2] - hdim;
+
+		// Print the header
+		cout << ":    Current position in ( z, y, x) is @(" << cz << ", " << cy << ", " << cx << ")" << endl;
+		cout << ":    Get to +-" << hdim << " on any axis to escape!" << endl;
+		cout << ":" << endl;
+		cout << ":    X      Z" << endl;
+
+
+
+		/*
+		 *	Print Orthographic projection for z, y, x sides
+		 */
 		// xside & zside >> single row at a time
 		for (int i = 0; i < dim; ++i) {
 			// Create x point
@@ -109,38 +128,14 @@ int printMaze(char maze[][3][3], int location[]) {
 		}
 
 		// Spacer
-		cout << ":" << endl << ":" << endl;
+		cout << ":" << endl;
+		cout << ":           Y" << endl;
 
 		for (int i = 0; i < dim; ++i) {
-			// Create location
-			int cz = location[0] - hdim;
-			int cy = location[1] - hdim;
-			int cx = location[2] - hdim;
-
-
 			// Create y point
 			int yz = zmin + i;
 			int yy = ymin;
 			int yx = location[2];
-
-			// Print spacer
-			cout << ":  ";
-
-			// Print location
-			switch (i) {
-			case 0:
-				cout << setw(13) << left << "  ( z,  y,  x)";
-				break;
-			case 1:
-				cout << setw(13) << left << " @(" << cz << ", " << cy << ", " << cx << ")";
-				break;
-			case 2:
-				cout << setw(13) << left << "achieve +-" << hdim;
-				break;
-			default:
-				cout << "Error: Bad case";
-				break;
-			}
 
 			// Print y row points
 			for (int b = 0; b < dim; ++b) {
